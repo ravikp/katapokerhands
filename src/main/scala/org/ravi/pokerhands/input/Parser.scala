fe802fca5b60530ac3d3d87fc41cd01ffcda8f54
Ravi Kumar Pasumarthy <ravikup@thoughtworks.com>
2015-12-04 11:26:16 +0800

    Improvde clarity: Add types for return parameters

--- src/main/scala/org/ravi/pokerhands/input/Parser.scala
+++ src/main/scala/org/ravi/pokerhands/input/Parser.scala
@@ -10,13 +10,18 @@ import scala.util.parsing.input.CharSequenceReader
  */
 
 object ParseInput extends JavaTokenParsers {
+
   def lineItem:Parser[List[Player]] = rep(player)
+
   def player:Parser[Player] = (playername <~ ":") ~ cards ^^ {
     case (playername ~ cards) => Player(playername, cards)
   }
-  def playername = "[a-zA-Z]+".r
-  def card = "[2-9TJQKA]".r ~ "[HDCS]".r ^^ {case value ~ cardtype => Card(value, cardtype)}
-  def cards = repN(5, card)
+
+  def playername:Parser[String] = "[a-zA-Z]+".r
+
+  def card:Parser[Card] = "[2-9TJQKA]".r ~ "[HDCS]".r ^^ {case value ~ cardtype => Card(value, cardtype)}
+
+  def cards:Parser[List[Card]] = repN(5, card)
 
   val lineItemPhrase = phrase(lineItem)
 
